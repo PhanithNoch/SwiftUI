fc6c982ec648a9537759700a3feeeda4b5597652
nochphanith <nochphanith@gmail.com>
2021-03-26 11:13:49 +0700

    Initial Commit

---
+++ LearnUI/LearnUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnUIApp.swift
+//  LearnUI
+//
+//  Created by Admin on 3/26/21.
+//
+
+import SwiftUI
+
+@main
+struct LearnUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
